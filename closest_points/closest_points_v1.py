d9f276ad532c82b82f2fe84484251653607af957
Ben <86641253+thejourneyville@users.noreply.github.com>
2021-07-09 23:48:52 +0900

    refactored
    cleaned up recursive function and studied the algorithm more

--- closest_points/closest_points_v1.py
+++ closest_points/closest_points_v1.py
@@ -3,7 +3,6 @@ from sys import maxsize
 
 # SORT ---------------------------
 def merge_sort_generator(A, x):
-
     def merge_sort(a, start, end):
 
         def merge(segment, s, m, e):
@@ -30,60 +29,51 @@ def merge_sort_generator(A, x):
         return a
 
     return merge_sort(list(A), 0, len(A) - 1)
+
+
 # --------------------------------
 
 
 # DISTANCE -----------------------
 def distance(point1, point2):
-
     x1, y1 = point1[0], point1[1]
     x2, y2 = point2[0], point2[1]
 
     return ((x2 - x1) ** 2 + (y2 - y1) ** 2) ** .5
+
+
 # --------------------------------
 
 
+# RECURSIVE FUNCTION -------------
 def divide_conquer_shortest_distance(coordinates_by_x):
 
     # base cases
-    if len(coordinates_by_x) <= 1:
-        return 0
-    elif len(coordinates_by_x) == 2:
-        return coordinates_by_x[0], coordinates_by_x[-1]
-    elif len(coordinates_by_x) == 3:
-        results = [(distance(coordinates_by_x[i], coordinates_by_x[(i + 1) % 3]),
-                    coordinates_by_x[i], coordinates_by_x[(i + 1) % 3]) for i in range(3)]
-        return min(results)[1:]
-
-    # recursively divide and conquer each half of x coordinates
-    else:
-        middle = len(coordinates_by_x) // 2
-
-        left = divide_conquer_shortest_distance(coordinates_by_x[:middle])
-        right = divide_conquer_shortest_distance(coordinates_by_x[middle:])
-
-        if distance(left[0], left[-1]) < distance(right[0], right[-1]):
-            delta = left
+    points = len(coordinates_by_x)
+    if points <= 3:
+        if points <= 1:
+            return 0
+        elif points == 2:
+            return distance(coordinates_by_x[0], coordinates_by_x[-1]), (coordinates_by_x[0], coordinates_by_x[-1])
         else:
-            delta = right
+            return merge_sort_generator([(distance(coordinates_by_x[idx], coordinates_by_x[(idx + 1) % 3]),
+                                          (coordinates_by_x[idx], coordinates_by_x[(idx + 1) % 3])) for idx in
+                                         range(3)], 0)[0]
 
-        return delta
+    # this will repeat recursively until len(coordinates_by_x) <= 3:
+    x_midpoint = points // 2
+    x_leftside = divide_conquer_shortest_distance(coordinates_by_x[:x_midpoint])
+    x_rightside = divide_conquer_shortest_distance(coordinates_by_x[x_midpoint:])
+
+    # find delta: the shortest distance pair of left and right sides
+    print(x_rightside)
+    if x_leftside[0] < x_rightside[0]:
+        delta = x_leftside
+    else:
+        delta = x_rightside
 
 
-
-
-
-
-
-
-
-test_list = ((2, 2), # A
-            (2, 8), # B
-            (5, 5), # C
-            (6, 3), # D
-            (6, 7), # E
-            (7, 4), # F
-            (7, 9)  )
+test_list = ((1, 2), (9, 11), (5, 20))
 sorted_x = merge_sort_generator(test_list, 0)
 print(sorted_x)
 print(divide_conquer_shortest_distance(sorted_x))
